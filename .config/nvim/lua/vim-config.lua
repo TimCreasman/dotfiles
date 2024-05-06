95dd745b1a154b090afbd0a84c544e1e247cb001
Tim Creasman <tcreasman18@gmail.com>
2024-05-06 14:32:49 -0400

    Updated shortcut for moving vertically using ctrl-d/u by appending zz Refactored tabs to default to 4 spaces

--- .config/nvim/lua/vim-config.lua
+++ .config/nvim/lua/vim-config.lua
@@ -1,11 +1,13 @@
--- default to 2 spaces for tab
+-- default to 4 spaces for tab
 vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
 
 -- combine the windows and wsl clipboards
 vim.cmd("set clipboard+=unnamedplus")
 
 vim.g.mapleader = " "
+vim.keymap.set({"n", "v"}, "<C-d>", "<C-d>zz")
+vim.keymap.set({"n", "v"}, "<C-u>", "<C-u>zz")
 
