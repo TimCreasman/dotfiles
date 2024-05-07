e3c7d0b6678c7305815e2b17d5051b038eeb286f
Tim Creasman <tcreasman18@gmail.com>
2024-05-07 16:37:51 -0400

    Show hidden files in neotree

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -1,27 +1,32 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		require("neo-tree").setup({
-			close_if_last_window = true,
-		})
-		vim.keymap.set("n", "<leader>n", ":Neotree<CR>", {})
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+        "MunifTanjim/nui.nvim",
+    },
+    config = function()
+        require("neo-tree").setup({
+            close_if_last_window = true,
+            filesystem = {
+                filtered_items = {
+                    visible = true,
+                },
+            },
+        })
+        vim.keymap.set("n", "<leader>n", ":Neotree<CR>", {})
 
-		-- auto open neo tree
-		vim.api.nvim_create_augroup("neotree", {})
-		vim.api.nvim_create_autocmd("UiEnter", {
-			desc = "Open Neotree automatically",
-			group = "neotree",
-			callback = function()
-				if vim.fn.argc() == 0 then
-					vim.cmd("Neotree toggle")
-				end
-			end,
-		})
-	end,
+        -- auto open neo tree
+        vim.api.nvim_create_augroup("neotree", {})
+        vim.api.nvim_create_autocmd("UiEnter", {
+            desc = "Open Neotree automatically",
+            group = "neotree",
+            callback = function()
+                if vim.fn.argc() == 0 then
+                    vim.cmd("Neotree toggle")
+                end
+            end,
+        })
+    end,
 }
