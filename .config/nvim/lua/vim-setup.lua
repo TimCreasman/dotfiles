73f030f76f610e080d0b70ec986779e8bed20727
Tim Creasman <tcreasman18@gmail.com>
2024-12-02 14:53:28 -0500

    Appended zz to ctrl o and i

--- .config/nvim/lua/vim-setup.lua
+++ .config/nvim/lua/vim-setup.lua
@@ -23,6 +23,9 @@ vim.g.mapleader = " "
 vim.keymap.set({ "n", "v" }, "<C-d>", "<C-d>zz")
 vim.keymap.set({ "n", "v" }, "<C-u>", "<C-u>zz")
 
+vim.keymap.set({ "n", "v" }, "<C-o>", "<C-o>zz")
+vim.keymap.set({ "n", "v" }, "<C-i>", "<C-i>zz")
+
 -- angular treesitter support setup
 vim.filetype.add({
     pattern = {
