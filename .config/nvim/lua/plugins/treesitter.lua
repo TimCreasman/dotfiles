f483d444adf6654bb594a4e41c8cb0d71bf5057d
Tim Creasman <tcreasman18@gmail.com>
2025-05-24 20:21:43 -0400

    Enabled textobject keymaps for treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,15 +1,42 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = function()
-		require("nvim-treesitter.install").update({ with_sync = true })()
-	end,
-	config = function()
-		local config = require("nvim-treesitter.configs")
-		config.setup({
-            ensure_installed = { "angular", "javascript", "typescript", "tsx", "css", "styled", "html" },
-			auto_install = true,
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end,
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = function()
+            require("nvim-treesitter.install").update({ with_sync = true })()
+        end,
+        event = { "BufReadPre", "BufNewFile" },
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter-textobjects",
+        },
+        config = function()
+            local config = require("nvim-treesitter.configs")
+            config.setup({
+                ensure_installed = { "angular", "javascript", "typescript", "tsx", "css", "styled", "html" },
+                auto_install = true,
+                highlight = { enable = true },
+                indent = { enable = true }
+            })
+        end,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        lazy = true,
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                textobjects = {
+                    select = {
+                        enable = true,
+                        lookahead = true,
+                        keymaps = {
+                            ["af"] = { query = "@function.outer", desc = "Select outer part of a function" },
+                            ["if"] = { query = "@function.inner", desc = "Select inner part of a function" },
+                            ["ac"] = { query = "@class.outer", desc = "Select outer part of a class region" },
+                            ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+                            ["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
+                        },
+                    }
+                }
+            })
+        end
+    }
 }
